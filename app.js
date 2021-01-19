cccd4d9aa11b8f1442ef8aa7220102691bf021a7
Ashish Dogra <ashishdogra1697@gmail.comgit config --global user.name Ashish>
2021-01-19 09:56:03 +0530

    second commit

--- app.js
+++ app.js
@@ -2,40 +2,13 @@
 
 $.getJSON("https://jsonplaceholder.typicode.com/photos", function(data){
   console.log(data);
-/* for(var i=0;i<data.length;i++){
+  var j='a';
+for(var i=0;i<data.length;i++){
     var image = data[i].url;
-    var id = "card" + i;
-    $('id').attr('src', image);
- }*/
-  var image = data[0].url;
-  $('#a').attr('src', image);
-  
-  var image = data[1].url;
-  $('#b').attr('src', image);
-
-  var image = data[2].url;
-  $('#c').attr('src', image);
-
-  var image = data[3].url;
-  $('#d').attr('src', image);
-
-  var image = data[4].url;
-  $('#e').attr('src', image);
-
-  var image = data[5].url;
-  $('#f').attr('src', image);
-
-  var image = data[6].url;
-  $('#g').attr('src', image);
-
-  var image = data[7].url;
-  $('#h').attr('src', image);
-
-  var image = data[8].url;
-  $('#i').attr('src', image);
-
+    $('#'+j).attr('src', image);
+    j=String.fromCharCode(j.charCodeAt(0)+1);
+ }
 });
-
 
 $("button").on("click",function(event){
     $(this).parent().fadeOut(500, function(){
